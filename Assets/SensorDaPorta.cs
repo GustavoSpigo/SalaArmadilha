64305b128a760f3063a614b593d9f4a8fed0a456
GUSTAVO CARVALHO GOMES DE ABREU <SESA193772@se.com>
2021-10-29 22:27:14 -0300

    Alterações feitas na aula de 2021-10-29

---
+++ Assets/SensorDaPorta.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorDaPorta : MonoBehaviour
+{
+    public Animator porta;
+    public Material corQuePode;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if ((other.CompareTag("Player")) || ((other.GetComponent<MeshRenderer>().materials[0].color.Equals(corQuePode.color))))
+        {
+            porta.SetBool("aberta", true);
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((other.CompareTag("Player")) || ((other.GetComponent<MeshRenderer>().materials[0].color.Equals(corQuePode.color))))
+        {
+            porta.SetBool("aberta", false);
+        }
+    }
+}
